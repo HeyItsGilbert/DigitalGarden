0ece0b31b1176d08da70870db3c32ec0bab8e804
Gilbert Sanchez <me@gilbertsanchez.com>
2024-01-21 08:47:42 -0800

    Fix FolderExplorer export in index.ts

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -20,7 +20,7 @@ import MobileOnly from "./MobileOnly"
 import RecentNotes from "./RecentNotes"
 import Breadcrumbs from "./Breadcrumbs"
 import Comments from "./Comments"
-import FolderExporer from "./FolderExplorer"
+import FolderExplorer from "./FolderExplorer"
 
 export {
   ArticleTitle,
@@ -45,4 +45,5 @@ export {
   NotFound,
   Breadcrumbs,
   Comments,
+  FolderExplorer,
 }

98ab94633bbb2888beddb3ad62858ccc3c47e0cf
Gilbert Sanchez <me@gilbertsanchez.com>
2024-01-20 18:54:21 -0800

    Update renderPage.tsx

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -208,9 +208,8 @@ export function renderPage(
                   ))}
                 </Header>
                 <div class="popover-hint">
-                  {beforeBody.map((BodyComponent) => (
-                    <BodyComponent {...componentData} />
-                  ))}
+                  {slug !== "index" &&
+                    beforeBody.map((BodyComponent) => <BodyComponent {...componentData} />)}
                 </div>
               </div>
               <Content {...componentData} />

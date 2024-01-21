19716b3eea3b640383036543e165e95892bb69d7
Gilbert Sanchez <me@gilbertsanchez.com>
2024-01-21 07:24:28 -0800

    Update comments to not lazy render

--- quartz/components/Comments.tsx
+++ quartz/components/Comments.tsx
@@ -8,14 +8,13 @@ export default (() => {
         data-repo-id="R_kgDOK_O8yA"
         data-category="Announcements"
         data-category-id="DIC_kwDOK_O8yM4CcHlb"
-        data-mapping="url"
+        data-mapping="pathname"
         data-strict="0"
         data-reactions-enabled="1"
         data-emit-metadata="0"
-        data-input-position="top"
+        data-input-position="bottom"
         data-theme="preferred_color_scheme"
         data-lang="en"
-        data-loading="lazy"
         crossorigin="anonymous"
         async>
       </script>

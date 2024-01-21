e33d9aabdda8de589a17da21faeba79b06780bf0
Gilbert Sanchez <me@gilbertsanchez.com>
2024-01-21 07:24:12 -0800

    Update links

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Blog: "https://gilbertsanchez.com",
+      LittleLink: "https://links.gilbertsanchez.com",
     },
   }),
 }
